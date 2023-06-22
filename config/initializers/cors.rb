1ffeb22455bdea257adb92ed5f72d6b98b1f97a4
Alcade <alcade.benjamin@gmail.com>
2023-06-22 14:55:52 +0200

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://main--helpful-parfait-ba2e59.netlify.app/'
+      origins '*'
       resource '*',
       headers: ["Authorization"],
       expose: ["Authorization"],
