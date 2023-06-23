e55ea74cff904a6b007e236d363602189647ccc4
Alcade <alcade.benjamin@gmail.com>
2023-06-23 14:15:30 +0200

    destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get 'private/user'
-  delete '/user/destroy', to: 'private#destroy'
   devise_for :users, 
     path: '', 
     path_names: {
       sign_in: 'login',
       sign_out: 'logout',
       registration: 'signup', 
-      
+      delete: 'destroy'
     },
     controllers: {
       sessions: 'users/sessions',
       registrations: 'users/registrations',
+      delete: 'users/sessions#destroy'
       
     }
     
