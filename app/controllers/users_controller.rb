82be6b865b0f6e5df615a5dd24bba19788337367
Alcade <alcade.benjamin@gmail.com>
2023-06-27 12:06:40 +0200

    userfix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
   def show
-    # Vérifiez le jeton d'authentification dans l'en-tête de la requête
     token = request.headers['Authorization']&.split(' ')&.last
     if token
-      # Décodage du jeton d'authentification  et récupération de l'ID de l'utilisateur
+
       user_id = JWT.decode(token, ENV['DEVISE_JWT_SECRET_KEY'])[0]['sub']
-      # Recherche de l'utilisateur par ID
+
       user = User.find_by(id: user_id)
       if user
         render json: user
@@ -13,7 +12,7 @@ class UsersController < ApplicationController
         render json: { error: 'Utilisateur introuvable' }, status: :not_found
       end
     else
-      render json: { error: 'Jetoon d\'authentification manquant' }, status: :unauthorized
+      render json: { error: 'Jeton d\'authentification manquant' }, status: :unauthorized
     end
   rescue JWT::DecodeError => e
     render json: { error: 'Jeton d\'authentification invalide' }, status: :unauthorized
